0eeefbb2eacfcaaaa97a19983c93697cb865495d
S.Ageev <Ageev.sa27@yandex.ru>
2024-05-28 01:14:59 +0300

    проект обновлен

--- src/test/java/steps/WireMockTestSteps.java
+++ src/test/java/steps/WireMockTestSteps.java
@@ -7,16 +7,8 @@ import org.junit.jupiter.api.Assertions;
 
 public class WireMockTestSteps {
 
-    //@Step()
-    // p s User qwe(){
-    //
-    // }
-
-
-    //@Step()
     public static User getUserFromStub(String userName){
 
-        user = qwe();
         ResponseDataContainer<User> response = UserService.getUserByName("Sergey");
         Assertions.assertEquals(200, response.getStatusCode());
         Assertions.assertNotNull(response.getData());
